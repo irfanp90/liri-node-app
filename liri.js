e80b2ec95fe7e25799175b1527cedf55312f05f9
irfanp90 <irfanp90@gmail.com>
2019-03-25 22:26:21 -0500

    update js

--- liri.js
+++ liri.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 var keys = require("./keys.js");
 var Spotify = require("node-spotify-api");
 var axios = require("axios");
+var fs = require("fs");
 var spotify = new Spotify(keys.spotify);
 
 var getArtistName = function(artist) {
@@ -29,9 +30,36 @@ var getMySpotify = function(songName) {
 var getMyMovie = function(movieName) {
   var queryUrl =
     "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-  console.log(queryUrl);
+  // console.log(queryUrl);
   axios.get(queryUrl).then(function(response) {
     console.log(response);
+    console.log("Title: " + response.data.Title);
+    console.log("Year: " + response.data.Year);
+    console.log("IMDB Rating: " + response.data.imdbRating);
+    console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1]);
+    console.log("Country: " + response.data.Country);
+    console.log("Languauge: " + response.data.Language);
+    console.log("Plot: " + response.data.Plot);
+    console.log("Actors: " + response.data.Actors);
+  });
+};
+var getMyConcert = function(artist) {
+  var queryURL =
+    "https://rest.bandsintown.com/artists/" + artist + "?app_id=codingbootcamp";
+  axios.get(queryURL).then(function(response) {
+    console.log(response);
+  });
+};
+var doWhatItSays = function() {
+  fs.readFile("random.txt", "utf8", function(error, data) {
+    if (error) {
+      return console.log(error);
+    }
+    // console.log(data);
+    var dataArr = data.split(",");
+    if (dataArr.length == 2) {
+      pick(dataArr[0], dataArr[1]);
+    }
   });
 };
 var pick = function(caseData, functionData) {
@@ -41,6 +69,12 @@ var pick = function(caseData, functionData) {
       break;
     case "movie-this":
       getMyMovie(functionData);
+      break;
+    case "concert-this":
+      getMyConcert(functionData);
+      break;
+    case "do-what-it-says":
+      doWhatItSays();
       break;
     default:
       console.log("DOES NOT KNOW");
